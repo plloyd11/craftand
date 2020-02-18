65e7503412abf178261fc35b880d5ee062dfe5fa
Pete Lloyd <plloyd11@gmail.com>
2020-02-17 21:46:04 -0500

    case studies nav good to go

--- src/components/CaseStudyNav.vue
+++ src/components/CaseStudyNav.vue
@@ -1,22 +1,38 @@
 <template>
   <section class="py-8 mt-24 bg-gray-200">
     <div class="container flex justify-between mx-auto">
-      <g-link
-        v-if="next"
-        :to="next"
-        class="flex items-center pl-4 text-sm uppercase lg:pl-12 din"
-      >
-        <LeftArrow class="w-3 pb-1 mr-1" />
-        Back
-      </g-link>
-      <g-link
-        v-if="prev"
-        :to="prev"
-        class="flex items-center pr-4 text-sm uppercase lg:pr-12 din"
-      >
-        Next
-        <RightArrow class="w-3 pb-1 ml-1" />
-      </g-link>
+      <button class="focus:outline-none">
+        <g-link
+          v-if="next"
+          :to="next"
+          class="flex items-center pr-4 text-sm uppercase lg:pl-12 din"
+        >
+          <LeftArrow class="w-3 pb-1 mr-1" />
+          Back
+        </g-link>
+        <p
+          v-else
+          class="flex items-center pr-4 text-sm uppercase cursor-not-allowed lg:pl-12 din"
+        >
+          Start of case studies
+        </p>
+      </button>
+      <button class="focus:outline-none">
+        <g-link
+          v-if="prev"
+          :to="prev"
+          class="flex items-center pr-4 text-sm uppercase lg:pr-12 din"
+        >
+          Next
+          <RightArrow class="w-3 pb-1 ml-1" />
+        </g-link>
+        <p
+          v-else
+          class="flex items-center pr-4 text-sm uppercase cursor-not-allowed lg:pr-12 din"
+        >
+          End of case studies
+        </p>
+      </button>
     </div>
   </section>
 </template>
