6dccb7d8ba347ebb5e0ad4a8d977b9ef3c3c4072
Pete Lloyd <plloyd11@gmail.com>
2020-07-22 10:37:36 -0400

    new thank you page

---
+++ src/pages/ThankYouSubscribe.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <section class="flex flex-col items-center w-full h-screen bg-primary-color">
+      <h1 class="max-w-xl pt-48 mx-auto text-2xl font-extrabold leading-none text-center lg:text-5xl proxima">
+        Thanks for subscribing!
+      </h1>
+      <g-link
+        to="/"
+        class="inline-block px-4 pt-4 pb-3 mt-16 text-sm tracking-wide text-center uppercase transition-all duration-300 ease-in-out bg-transparent border border-black cursor-pointer hover:bg-main-gray hover:text-white din"
+      >
+        Back to the site
+      </g-link>
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: 'Thank You',
+    meta: [
+      { name: 'description', content: 'Thank you for filling out our form!' }
+    ]
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
